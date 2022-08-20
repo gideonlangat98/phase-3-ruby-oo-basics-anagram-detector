ac73f2f39a6f9f23b94413626895377596ff14f3
gideonlangat98 <gideonlangat98@gmail.com>
2022-08-20 13:37:20 +0300

    commit tree ccdcd750daeb84732d726cd4e3f3a70b77dd8423

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+    end
+
+    def match
+        result = {}
+        words = matches.each do |match|
+            key = word.split('').sort.join
+            if result.has_key?(key)
+                results.[key].push(word)
+            else
+                result = [word]
+            end
+        end
+        results.each do |k, v|
+            puts "No matches"
+    end
+end
